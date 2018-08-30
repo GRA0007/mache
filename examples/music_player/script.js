0fa389c74da4093f3d4de108a968ca5e1cb01097
Benjamin Grant <benjamin.grantGRA0007@gmail.com>
2018-08-30 17:25:06 +1000

    Example relocation and updated functionality

---
+++ examples/music_player/script.js
@@ -0,0 +1,4 @@
+// When #open_sheet clicked, toggle the class .extended on #media
+document.getElementById('open_sheet').onclick = function() {
+	document.getElementById('media').classList.toggle('extended');
+};
